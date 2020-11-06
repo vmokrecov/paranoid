bfcf44cfa28eca5bf6d93da9217873029009aa72
viktormokrecov <viktormokrecov@gmail.com>
2020-11-06 13:09:35 +0300

    init commit

---
+++ src/test/java/com/vmokrecov/paranoid/ParanoidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vmokrecov.paranoid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParanoidApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
